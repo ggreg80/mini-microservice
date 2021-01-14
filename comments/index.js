735a43a69c38fcc58ff8c6077f523a927356b02f
ggreg80 <ggreg80@outlook.com>
2021-01-14 18:08:56 -0300

    Receiving Events

--- comments/index.js
+++ comments/index.js
@@ -24,7 +24,7 @@ app.post('/posts/:id/comments', async (req, res) => {
 
     commentsByPostId[req.params.id] = comments
 
-    await axios.post('http://localhost:4005/event', {
+    await axios.post('http://localhost:4005/events', {
         type: 'CommentCreated',
         data: {
             id: commentId,
@@ -34,6 +34,12 @@ app.post('/posts/:id/comments', async (req, res) => {
     })
 
     res.status(201).send(comments)
+})
+
+app.post('/events', (req, res) => {
+    console.log('Received Event', req.body.type)
+
+    res.send({})
 })
 
 app.listen(4001, () => {
