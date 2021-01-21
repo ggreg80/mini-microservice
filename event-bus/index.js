607f9c4bc288f709cc089c21f8411faeb1e57be7
ggreg80 <ggreg80@outlook.com>
2021-01-20 23:04:34 -0300

    Event-bus Fix

--- event-bus/index.js
+++ event-bus/index.js
@@ -12,7 +12,7 @@ app.post('/events', async (req, res) => {
 
     events.push(event)
 
-    await axios.post('http://posts-cluterip-srv:4000/events', event)
+    await axios.post('http://posts-clusterip-srv:4000/events', event)
     //await axios.post('http://localhost:4001/events', event)
     //await axios.post('http://localhost:4002/events', event)
     //await axios.post('http://localhost:4003/events', event)
